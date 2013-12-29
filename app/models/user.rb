68a408786bf39c0158a6cdaaf03fbe129e9cbece
Michael Morales <morales_michael@fastmail.fm>
2013-12-28 17:43:21 -0700

    Styled the flash messages.

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ActiveRecord::Base
   validates :profile_name, presence: true,
                            uniqueness: true,
                            format: {
-                                with: /a-zA-Z0-9_-/,
+                                with: /\A[a-zA-Z\-\_]+\Z/,
                            message: 'Must be formatted correctly.'
                            }
 
