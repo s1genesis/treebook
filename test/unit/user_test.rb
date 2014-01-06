d9d391f69f6c0dd0aa2eec50fea7f8dd84a91f4d
Michael Morales <morales_michael@fastmail.fm>
2014-01-05 23:31:32 -0700

    Added shoulda to test group

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,6 +1,9 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+    should have_many(:user_friendships)
+    should have_many(:friends)
+  
     test "a user should enter a first name" do
     user = User.new
     assert !user.save
@@ -46,4 +49,20 @@ class UserTest < ActiveSupport::TestCase
      user.profile_name = 'djmike'
      assert user.valid?
   end
+   
+  test "that no error is raised when trying to access a friend list" do
+     assert_nothing_raised do
+        users(:michael).friends
+     end
 end
+
+   test "that creating friendships on a user works" do
+     users(:michael).friends << users(:virginia)
+     users(:michael).friends.reload
+     assert users(:michael).friends.include?(users(:virginia))
+   end
+end
+
+     
+     
+
